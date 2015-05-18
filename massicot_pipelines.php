019368b7cfbb4225861c6740819d481e97ef52e0
Michel Bystranowski <michel@vertige.org>
2015-05-18 17:14:17 +0200

    Ajoute le slider jQuery UI

--- massicot_pipelines.php
+++ massicot_pipelines.php
@@ -11,7 +11,16 @@
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 	
+/**
+ * Ajoute le plugins jqueryui Slider
+ *
+ * @pipeline jqueryui_plugins
+ * @param  array $scripts  Plugins jqueryui à charger
+ * @return array       Liste des plugins jquerui complétée
+ */
+function massicot_jqueryui_plugins ($scripts) {
 
+    $scripts[] = 'jquery.ui.slider';
 
-
-?>
+    return $scripts;
+}
